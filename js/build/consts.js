90664a9fd5330ccb2f6e8e97a35c95eecd34d5a5
Bjorkbat <bjorkbat@gmail.com>
2016-07-16 16:25:49 -0600

    everything else

---
+++ js/build/consts.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/**
+ * Useful constants and shit
+ * * * * */
+
+var GROUND_WIDTH = exports.GROUND_WIDTH = 50;
+var BLOCK_WIDTH = exports.BLOCK_WIDTH = 2.5;
---
+++ js/src/consts.js
@@ -0,0 +1,6 @@
+/**
+ * Useful constants and shit
+ * * * * */
+
+export const GROUND_WIDTH = 50;
+export const BLOCK_WIDTH = 2.5;
